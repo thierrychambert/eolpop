f87533e75beac490ab55c3f5e0cbba2925db6285
thierrychambert <thierry.chambert@gmail.com>
2021-10-08 10:54:15 +0200

    Added custom color palette

---
+++ R/custom_palette.R
@@ -0,0 +1,25 @@
+#' Custom color palette: 25 distinct colors
+#'
+#' @return a vector of 25 color names
+#' @export
+#'
+#' @examples
+#' custom_palette_c25()
+custom_palette_c25 <- function(){
+  c("black",
+  "dodgerblue2", "#E31A1C", # red
+  "green4",
+  "#6A3D9A", # purple
+  "#FF7F00", # orange
+  "gold1",
+  "skyblue2", "#FB9A99", # lt pink
+  "palegreen2",
+  "#CAB2D6", # lt purple
+  "#FDBF6F", # lt orange
+  "gray70", "khaki2",
+  "maroon", "orchid1", "deeppink1", "blue1", "steelblue4",
+  "darkturquoise", "green1", "yellow4", "yellow3",
+  "darkorange4", "brown"
+  )
+}
+
