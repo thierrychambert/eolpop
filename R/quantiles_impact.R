33ad2570eabbf6806f8d7d64aa8f3512a9f46c6a
thierrychambert <thierry.chambert@gmail.com>
2021-11-23 16:45:01 +0100

    Updated quantile impact function (to allow individual wind_farm impacts)

--- R/quantiles_impact.R
+++ R/quantiles_impact.R
@@ -1,6 +1,6 @@
 #' Calculate quantile values of impact at time horizon
 #'
-#' @param N a 4-D array containing demographic projection outputs
+#' @param dr_N a 3-D array (year, nsim, nsc) containing the output : relative difference of population size
 #' @param show_quantile value between 0 and 1. The quantile to of the CDF to calculate
 #' @param show_CI value between 0 and 1. The limits of C.I. to calculate
 #' @param percent a logical value indicating whether the impact should be displayed in % (y axis).
@@ -10,17 +10,17 @@
 #' @export
 #'
 #'
-quantiles_impact <- function(N, show_quantile = 0.975, show_CI = 0.95, percent = TRUE){
+quantiles_impact <- function(dr_N, show_quantile = 0.975, show_CI = 0.95, percent = TRUE){
 
-  if(percent) out <- get_metrics(N)$scenario$DR_N*100 else out <- get_metrics(N)$scenario$DR_N
+  if(percent) dr_N <- dr_N*100
 
   CI <- QT <- NA
 
   TH <- dim(N)[2]
   nsc <- dim(N)[3]
 
-  if(!is.null(show_CI)) CI <- apply(-out[TH,,], 2, quantile, probs = c(0.5, (1-show_CI)/2, 1-(1-show_CI)/2))
-  if(!is.null(show_quantile)) QT <- apply(-out[TH,,], 2, quantile, probs = show_quantile)
+  if(!is.null(show_CI)) CI <- apply(-dr_N[TH,,], 2, quantile, probs = c(0.5, (1-show_CI)/2, 1-(1-show_CI)/2))
+  if(!is.null(show_quantile)) QT <- apply(-dr_N[TH,,], 2, quantile, probs = show_quantile)
 
   return(list(CI = CI, QT = QT))
 }
