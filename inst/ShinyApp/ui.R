13127875aacf4505a1ae26c73c5680fd95a0bcb6
thierrychambert <thierry.chambert@gmail.com>
2022-03-04 14:04:05 +0100

    EolPop v1.2

--- inst/ShinyApp/ui.R
+++ inst/ShinyApp/ui.R
@@ -75,7 +75,7 @@ rm(list = ls(all.names = TRUE))
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
 # Define User Interface ########################################################
 {ui <- navbarPage(
-  "EolPop v1.1",
+  "EolPop v1.2",
 
   # Graphic Design Parameters
   header =
